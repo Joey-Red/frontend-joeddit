acdad45af5774eb75694dbc7cd6f6ac302602c62
Joey <xjoey96@gmail.com>
2023-02-08 05:35:33 -0600

    Setup tailwind, main components, state, contextEtc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useMemo } from "react";
+import CreatePost from "./components/CreatePost";
+import HomeContainer from "./components/HomeContainer";
+import Nav from "./components/Nav";
+import { UserContext } from "./context/UserContext";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ViewPost from "./components/ViewPost";
+import Contact from "./components/Contact";
+import LogInModal from "./components/LogInModal";
+import { LogInModalContext } from "./context/LogInModalContext";
+import { SignUpModalContext } from "./context/SignUpModalContext";
+import SignUpModal from "./components/SignUpModal";
+import CommunityPage from "./components/CommunityPage";
+import PersonalPage from "./components/PersonalPage";
+import Settings from "./components/Settings";
+import { TempLoginPw } from "./context/TempLoginPwContext";
+import { TempLoginUN } from "./context/TempLoginUNContext";
+// import HomeContainerTESTING from "./components/HomeContainerTESTING";
 function App() {
+  const [user, setUser] = useState(null);
+  const [showLogin, setShowLogin] = useState(false);
+  const [showSignUp, setShowSignUp] = useState(false);
+  const [tempPw, setTempPw] = useState("");
+  const [tempUN, setTempUN] = useState("");
+  const userValue = useMemo(
+    () => ({
+      user,
+      setUser,
+    }),
+    [user, setUser]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UserContext.Provider value={userValue}>
+        <TempLoginPw.Provider value={{ tempPw, setTempPw }}>
+          <TempLoginUN.Provider value={{ tempUN, setTempUN }}>
+            <LogInModalContext.Provider value={{ showLogin, setShowLogin }}>
+              <SignUpModalContext.Provider
+                value={{ showSignUp, setShowSignUp }}
+              >
+                <Router basename={`/${process.env.PUBLIC_URL}`}>
+                  <Nav />
+                  {showLogin && <LogInModal />}
+                  {showSignUp && <SignUpModal />}
+                  <div>
+                    <Routes>
+                      {/* <Route path="/" element={<HomeContainerTESTING />}></Route> */}
+                      <Route path="/" element={<HomeContainer />}></Route>
+                      <Route
+                        path="/create-post"
+                        element={<CreatePost />}
+                      ></Route>
+                      <Route
+                        path="/retrieve-post/:id"
+                        element={<ViewPost />}
+                      ></Route>
+                      <Route
+                        path="/retrieve-post/undefined"
+                        element={<HomeContainer />}
+                      ></Route>
+                      <Route path="/contact" element={<Contact />}></Route>
+                      <Route
+                        path="/community/:id"
+                        element={<CommunityPage />}
+                      ></Route>
+                      <Route path="/u/:id" element={<PersonalPage />}></Route>
+                      <Route path="/u/settings" element={<Settings />}></Route>
+                    </Routes>
+                  </div>
+                </Router>
+              </SignUpModalContext.Provider>
+            </LogInModalContext.Provider>
+          </TempLoginUN.Provider>
+        </TempLoginPw.Provider>
+      </UserContext.Provider>
     </div>
   );
 }
