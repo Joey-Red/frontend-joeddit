880d103481fe04e8c68350baab44f635dd2d8bc2
Joey <xjoey96@gmail.com>
2023-02-08 05:35:46 -0600

    Strictly for testing

---
+++ src/components/HomeContainer222.js
@@ -0,0 +1,58 @@
+import React, { useContext, useEffect, useState } from "react";
+import StandardPost from "./StandardPost";
+import SideFeed from "./SideFeed";
+import { LogInModalContext } from "../context/LogInModalContext";
+import { SignUpModalContext } from "../context/SignUpModalContext";
+import axios from "axios";
+function HomeContainer() {
+  let [fetchPosts, setFetchedPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/retrieve-posts", {})
+      .then((res) => {
+        if (res.status === 200) {
+          setFetchedPosts(res.data);
+        }
+      })
+      .catch();
+  }, []);
+
+  const { showLogin, setShowLogin } = useContext(LogInModalContext);
+  const { showSignUp, setShowSignUp } = useContext(SignUpModalContext);
+  useEffect(() => {
+    let toggle = document.getElementById("toggle");
+    if (showLogin || showSignUp) {
+      toggle.classList.add("max-h-trueMax", "overflow-hidden");
+    } else if (toggle.classList.contains("max-h-trueMax", "overflow-hidden")) {
+      toggle.classList.remove("max-h-trueMax", "overflow-hidden");
+    }
+  }, [showLogin, setShowLogin, showSignUp, setShowSignUp]);
+
+  return (
+    <div id="toggle">
+      <div className="flex justify-center px-6 py-4 bg-stone-300/50  min-h-trueMax">
+        <div className="flex flex-col">
+          {/*  */}
+          {/* w-160 */}
+          <button className="overflow-hidden w-160 relative mb-4 bg-white rounded p-2 h-14 flex justify-center items-center">
+            <a
+              href="/create-post"
+              className="text-xl text-white z-10 rounded p-1 bg-red-400 top-0 left-0 right-0 bottom-0 absolute flex justify-center items-center border-gray-900/30 border-[1px]"
+            >
+              Create a Post & Join the Conversation
+            </a>
+          </button>
+          {fetchPosts.map((post) => {
+            return <StandardPost post={post} key={post._id} />;
+          })}
+        </div>
+        <div className="hidden lg:flex flex-col">
+          <SideFeed />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomeContainer;
